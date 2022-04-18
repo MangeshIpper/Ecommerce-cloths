95bc5927c4582ac4b71032e0f79ea559d436fca3
MangeshIpperNCI <mangesh.ipper@gmail.com>
2022-04-18 10:39:11 +0100

    Added firebase authentication.

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,53 @@
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  signInWithRedirect,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from 'firebase/auth';
+import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC5vvWbrfbfz8WfJ9hOynL9CLZuNKm92HY",
+  authDomain: "ecommerce-cloths-db.firebaseapp.com",
+  projectId: "ecommerce-cloths-db",
+  storageBucket: "ecommerce-cloths-db.appspot.com",
+  messagingSenderId: "731618982414",
+  appId: "1:731618982414:web:35b7da9e750f0f9f8f1a5f"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+  prompt: 'select_account',
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
+
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createdAt,
+      });
+    } catch (error) {
+      console.log('error creating the user', error.message);
+    }
+  }
+
+  return userDocRef;
+};
